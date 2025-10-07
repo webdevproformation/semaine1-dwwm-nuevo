ab2e5e4d529486d11bcb32e149c8b75f91d94905
Malik H <malik.h@webdevpro.net>
2025-10-07 15:48:16 +0200

    les balises html de formulaire

---
+++ jour02/fichier.php
@@ -0,0 +1,26 @@
+<?php 
+
+if(isset($_POST["envoyé"])){
+
+    // récupérer les données 
+
+    [
+        "nom" => $nom ,
+        "message" => $message,
+        "email" => $email
+
+    ] = $_POST ;
+
+    // vérification 
+    $verif = [];
+    if(strlen($message) < 10){
+        $verif[] = "nb caractère message est trop faible !!" ;
+    }
+
+    if(count($verif) === 0 ){
+
+        mail();
+
+        // INSERT INTO .... 
+    }
+}
